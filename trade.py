43f92dee25c7df98c8e82ad261357c011f1f9781
Nikolay Marchuk <marchuk.nikolaay@gmail.com>
2024-10-16 18:48:55 +0300

    move functions from main to trade

--- trade.py
+++ trade.py
@@ -94,3 +94,58 @@ def get_short_winrate(df):
         int: The win rate for 'Short' entries as a percentage.
     """
     return get_winrate(df, 'Short')
+
+def get_take_profit(entry_price, stop_loss, coefficient):
+    """
+    Calculates the take profit level based on the entry price, stop loss, and a risk reward coefficient.
+
+    Parameters:
+        entry_price (float): The entry price of the trade.
+        stop_loss (float): The stop loss price.
+        coefficient (float): The risk reward coefficient.
+
+    Returns:
+        float: The calculated take profit price.
+    """
+    diff = coefficient * abs(entry_price - stop_loss)
+    return entry_price - diff if entry_price < stop_loss else entry_price + diff
+
+def get_trade_result(df, entry_type, take_profit, stop_loss):
+    """
+    Determines the result of a trade based on the entry type, take profit, and stop loss levels.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing market data with 'High', 'Low', and 'Open Time' columns.
+        entry_type (str): The type of trade entry ('Long' or 'Short').
+        take_profit (float): The take profit price level.
+        stop_loss (float): The stop loss price level.
+
+    Returns:
+        dict or None: A dictionary containing the trade result and result date, or None if no result.
+    """
+    # Set conditions based on entry type
+    if entry_type == 'Short':
+        tp_condition = df['Low'] <= take_profit
+        sl_condition = df['High'] >= stop_loss
+    elif entry_type == 'Long':
+        tp_condition = df['High'] >= take_profit
+        sl_condition = df['Low'] <= stop_loss
+    else:
+        return None  # Invalid entry type
+
+    # Find the first occurrence of take profit or stop loss
+    take_profit_reached = df[tp_condition].reset_index(drop=True).iloc[0] if not df[tp_condition].empty else None
+    stop_loss_reached = df[sl_condition].reset_index(drop=True).iloc[0] if not df[sl_condition].empty else None
+
+    # Determine the trade outcome
+    if take_profit_reached is not None and stop_loss_reached is not None:
+        if take_profit_reached['Open Time'] < stop_loss_reached['Open Time']:
+            return {'Result': 'Win', 'Result Date': take_profit_reached['Open Time']}
+        else:
+            return {'Result': 'Lose', 'Result Date': stop_loss_reached['Open Time']}
+    elif take_profit_reached is not None:
+        return {'Result': 'Win', 'Result Date': take_profit_reached['Open Time']}
+    elif stop_loss_reached is not None:
+        return {'Result': 'Lose', 'Result Date': stop_loss_reached['Open Time']}
+    
+    return None
